1f9e7286683cc071ce350031361b5cdafc65d202
Johannes Haseitl <johannes@undpaul.de>
2013-03-30 17:27:54 +0100

    Generic symlinking through environment params.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Drupal update_sh
+
+This is a basic template of an update script we use nowerdays for updating all
+parts of the Drupal installation after a code update has made in the repository.
+It is used on the local environment and the integration, test and live site to
+ensure different tasks have been executed.
+
+## Tasks
+
+Tasks may be any deployment related stuff Drupal needs to ensure the site is
+rolled out correctly and exactly behaves like intended.
--- update/002_symlinks.sh
+++ update/002_symlinks.sh
@@ -1,31 +1,39 @@
 
 # Ensure we got all symlinks on the local system we need to have.
 
+# We quit this script part if no stage indicator was set.
+if [ "$STAGE_INDICATOR" == "" ]
+then
+  echo "No stage indicator given                                      [status]"
+  exit
+fi
+
 # Go to the sites/default folder.
 cd $DRUPAL_SITE
 
-# Ensure the settings.php is correctly smylinked.
+# Ensure the settings.php is available.
 if [ ! -f settings.php ]
 then
-  ln -s settings.live.php settings.php
+  ln -s "settings.$STAGE_INDICATOR.php" settings.php
 else
   echo "settings.php already present"
 fi
 
-# Make sure the files directory was created and has appropriate rights.
+# Ensure the files directory exists and has proper rights.
 if [ ! -d files ]
 then
   mkdir files
 else
   echo "files already exists"
 fi
+# Ensure files can be written.
 chmod 777 files
 
 # Navigating to root so we can set the .htaccess symlink.
 cd $DRUPAL_ROOT
 if [ ! -f .htaccess ]
 then
-  ln -s .htaccess.live .htaccess
+  ln -s ".htaccess.$STAGE_INDICATOR" .htaccess
 else
   echo ".htaccess already exists"
 fi
