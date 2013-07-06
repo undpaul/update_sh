e5d2559e6f75ccbb9a1e18f0c55cd03b0c6c8506
Johannes Haseitl <johannes@undpaul.de>
2013-07-06 16:41:56 +0200

    Variables from settings.php always take precedence over db storage.
    @see https://api.drupal.org/api/drupal/includes%21bootstrap.inc/function/variable_initialize/7

--- update/060_ensure_settings_php.sh
+++
@@ -1,7 +0,0 @@
-################################################################################
-# We can ensure to load variables from settings.php by deleting the variable
-# from the variable table.
-################################################################################
-
-# Disable update email notification.
-drush $DRUSH_PARAMS --yes vdel update_notify_emails
