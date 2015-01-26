fbc3802165c12a5116094daca4e034defe80e842
Thomas Keitel <tom@undpaul.de>
2015-01-26 15:51:41 +0100

    Reset symlinks of settings.php and .htaccess files on every update.sh run
    The symlinks are regenerated during each update.sh run and potential existing files are backed up

--- update/005_symlinks.sh
+++ update/005_symlinks.sh
@@ -2,6 +2,8 @@
 # Ensure we got all symlinks on the stage we currently are.
 ################################################################################
 
+readonly TIMESTAMP=$(date +%s);
+
 # We quit this script part if no stage indicator was set.
 if [ "$STAGE_INDICATOR" == "" ]
 then
@@ -12,16 +14,22 @@ fi
 # Go to the sites/default folder.
 cd $DRUPAL_SITE
 
-# Ensure the settings.php is available.
-if [ ! -f settings.php ]
+# Make sure the settings.php is allways set to the correct symlink.
+# In case we already got a symlink, we delete it and reset it again.
+if [ -L settings.php ]
 then
-  ln -s "settings.$STAGE_INDICATOR.php" settings.php
-elif [ -L settings.php ]
+  rm settings.php;
+  echo "Resetting symlink for settings.php ..."
+# In case it is no symlink, we back it up
+elif [ -f settings.php ]
 then
-  echo "settings.php already present as symlink"
+  mv settings.php "settings.$TIMESTAMP.bak.php";
+  echo "settings.php is not a symlink. Backing it up now ...";
 else
-  echo "settings.php already exists but is not a symlink                                                     [warning]"
+  echo "Setting symlink for settings.php";
 fi
+# Finally the symlink will be set.
+ln -s "settings.$STAGE_INDICATOR.php" settings.php
 
 # Ensure the files directory exists and has proper rights.
 if [ ! -d files ]
@@ -35,9 +43,20 @@ chmod 777 files
 
 # Navigating to root so we can set the .htaccess symlink.
 cd $DRUPAL_ROOT
-if [ ! -f .htaccess ]
+
+# Make sure the .htaccess is allways set to the correct symlink.
+# In case we already got a symlink, we delete it and reset it again.
+if [ -L .htaccess ]
 then
-  ln -s ".htaccess.$STAGE_INDICATOR" .htaccess
+  rm .htaccess;
+  echo "Resetting symlink for .htaccess ..."
+# In case it is no symlink, we back it up
+elif [ -f .htaccess ]
+then
+  mv .htaccess ".htaccess.$TIMESTAMP.bak";
+  echo ".htaccess is not a symlink. Backing it up now ...";
 else
-  echo ".htaccess already exists"
+  echo "Setting symlink for .htaccess";
 fi
+# Finally the symlink will be set.
+ln -s ".htaccess.$STAGE_INDICATOR" .htaccess
